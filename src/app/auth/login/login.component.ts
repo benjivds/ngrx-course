1e0adbc3c89b1212b990cbbf0f187699493da442
benjivds <benji_vds@yahoo.com>
2019-09-24 14:28:33 +0000

    Defining NgRx Actions using Action Creators

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -9,6 +9,8 @@ import {tap} from "rxjs/operators";
 import {noop} from "rxjs";
 import {Router} from "@angular/router";
 import {AppState} from '../../reducers/index';
+import {login} from '../auth.actions';
+
 
 @Component({
   selector: 'login',
@@ -43,6 +45,7 @@ export class LoginComponent implements OnInit {
       .pipe(
         tap(user => {
           console.log(user);
+          this.store.dispatch(login({user}));
           this.router.navigateByUrl('/courses');
         })
       )
