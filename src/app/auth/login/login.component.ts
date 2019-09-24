f209745597e4862e7fa25ef70e31d39e81d95ba5
benjivds <benji_vds@yahoo.com>
2019-09-24 14:12:52 +0000

    creating the store service api

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,10 +3,12 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 import {Store} from "@ngrx/store";
 
+
 import {AuthService} from "../auth.service";
 import {tap} from "rxjs/operators";
 import {noop} from "rxjs";
 import {Router} from "@angular/router";
+import {AppState} from '../../reducers/index';
 
 @Component({
   selector: 'login',
@@ -20,8 +22,9 @@ export class LoginComponent implements OnInit {
   constructor(
       private fb:FormBuilder,
       private auth: AuthService,
-      private router:Router) {
-
+      private router:Router,
+      private store: Store<AppState>
+      ) {
       this.form = fb.group({
           email: ['test@angular-university.io', [Validators.required]],
           password: ['test', [Validators.required]]
@@ -34,6 +37,19 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
+      const val = this.form.value;
+
+      this.auth.login(val.email, val.password)
+      .pipe(
+        tap(user => {
+          console.log(user);
+          this.router.navigateByUrl('/courses');
+        })
+      )
+      .subscribe(
+        noop,
+        () => alert('Login Failed')
+      )
 
   }
 
