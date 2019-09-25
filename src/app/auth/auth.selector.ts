5bd32e9a1ffbece4c8c83e6f6cb38ff4babb393b
benjivds <benji_vds@yahoo.com>
2019-09-25 11:27:02 +0000

    NgRx Feature Selectors - a Simple Explanation

--- src/app/auth/auth.selector.ts
+++ src/app/auth/auth.selector.ts
@@ -1,8 +1,16 @@
-import { createSelector } from "@ngrx/store";
+import { createSelector, createFeatureSelector } from "@ngrx/store";
+import { AuthState } from "./reducers";
 
+// feature selector to make store typesafe
+export const selectAuthState = createFeatureSelector<AuthState>("auth");
 // memorized function only execute when the input changes
-export const isLoggedIn = createSelector(
+/*export const isLoggedIn = createSelector(
   state => state["auth"],
+  (auth) => !!auth.user
+);*/
+
+export const isLoggedIn = createSelector(
+  selectAuthState,
   (auth) => !!auth.user
 );
 
