7a7699dfa8dd2c10c6d43e9855ca9acc2365a4f5
benjivds <benji_vds@yahoo.com>
2019-09-25 12:12:41 +0000

    NgRx Effects - Step-by-Step Implementation

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,13 +1,30 @@
 import { Injectable } from "@angular/core";
-import { Actions } from "@ngrx/effects";
+import { Actions, ofType, createEffect } from "@ngrx/effects";
+import { filter } from "minimatch";
+import { AuthActions } from "./action-types";
+import { tap } from "rxjs/operators";
 
 @Injectable()
 export class AuthEffects {
-    constructor(private actions$: Actions){
-        actions$.subscribe(action => {
-          if(action.type === '[Login Page] User Login'){
-            localStorage.setItem('user',JSON.stringify(action["user"]));
-          }
-        });
-    }
+  login$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.login),
+      tap(action => {
+        localStorage.setItem('user', JSON.stringify(action.user));
+      })
+    )
+    ,
+    {dispatch: false}
+    // to make not a dispatch when the login action else it will
+    // dispatch other event of login
+  );
+
+  constructor(private actions$: Actions) {
+    // login$.subscribe();
+    /*  actions$.subscribe(action => {
+        if(action.type === '[Login Page] User Login'){
+          localStorage.setItem('user',JSON.stringify(action["user"]));
+        }
+      });*/
+  }
 }
