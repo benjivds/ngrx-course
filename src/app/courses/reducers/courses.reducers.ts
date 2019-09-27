34adb82bc5b3fa5e0798a4d3de5737610e329635
benjivds <benji_vds@yahoo.com>
2019-09-27 13:08:57 +0000

    ngrx-entity-adapter-courses

--- src/app/courses/reducers/courses.reducers.ts
+++ src/app/courses/reducers/courses.reducers.ts
@@ -1,6 +1,25 @@
 import { Course } from "../model/course";
+import { EntityState, createEntityAdapter } from "@ngrx/entity";
+import { createReducer, on } from "@ngrx/store";
+
+import { CourseActions } from "../courses.action-type";
 
 
-export interface CourseStae {
-  entities: { [key: number]: Course }
+
+
+
+export interface CourseState extends EntityState<Course> {
+  /* entities: { [key: number]: Course },
+   ids: number[]*/
 }
+
+export const adapter = createEntityAdapter<Course>();
+
+export const initialCoursesState = adapter.getInitialState();
+
+export const coursesReducer = createReducer(
+  initialCoursesState,
+  on(CourseActions.allCoursesLoaded,
+    (state, action) => adapter.addAll(action.courses, state))
+);
+
